199e0461f5150cb5d943701a58e2c6c894a58473
Gabriele <gabriele.valvano@gmail.com>
2018-12-11 16:46:17 +0100

    added utils

---
+++ idas/utils.py
@@ -0,0 +1,26 @@
+import os
+from tensorflow.python.client import device_lib
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+def get_available_cpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'CPU']
+
+
+def safe_mkdir(path):
+    """ Create a directory if there isn't one already. """
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
+
+
+def print_yellow_text(text):
+    print('_' * 40 + '\n\033[1;33m{0}:\033[0m'.format(text))
+
+

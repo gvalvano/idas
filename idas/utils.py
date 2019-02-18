482efcc2ed3c59b8d1e6809eaa0bdad4118db646
Gabriele Valvano <gabrielevalvano@MacBook-Pro-di-Gabriele.local>
2019-02-18 15:51:19 +0100

    added utility to limit GPU fraction

--- idas/utils.py
+++ idas/utils.py
@@ -12,6 +12,18 @@ def get_available_cpus():
     return [x.name for x in local_device_protos if x.device_type == 'CPU']
 
 
+def set_gpu_fraction(gpu_fraction=0.8):
+    """ Set the GPU memory fraction for the application. 
+    References: `TensorFlow using GPU <https://www.tensorflow.org/versions/r0.9/how_tos/using_gpu/index.html>`
+    :param gpu_fraction : (float) Fraction of GPU memory, (0 ~ 1]
+    :return: -
+    """
+    tl.logging.info("[TL]: GPU MEM Fraction %f" % gpu_fraction)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    return sess
+
+
 def safe_mkdir(path):
     """ Create a directory if there isn't one already. """
     try:

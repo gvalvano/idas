8f9d1aed4fd422a37d7828317ad7204327951ac0
gabrielevalvano <gabriele.valvano@gmail.com>
2019-03-08 16:53:07 +0100

    fix syntax error

--- idas/utils.py
+++ idas/utils.py
@@ -19,7 +19,7 @@ def set_gpu_fraction(gpu_fraction=0.8):
     :param gpu_fraction : (float) Fraction of GPU memory, (0 ~ 1]
     :return: -
     """
-    tl.logging.info("[TL]: GPU MEM Fraction %f" % gpu_fraction)
+    tf.logging.info("[TL]: GPU MEM Fraction %f" % gpu_fraction)
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
     return sess

261df667a823679194da3e3e251b8271351d8712
gabrielevalvano <gabriele.valvano@gmail.com>
2019-05-23 12:23:24 +0100

    add progress bar

--- idas/utils.py
+++ idas/utils.py
@@ -55,3 +55,96 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+class ProgressBar(object):
+    def __init__(self, update_delay):
+        """
+        Progress bar.
+        :param update_delay: (int) time to wait before updating the progress bar (in seconds).
+
+        ----------
+        Example 1:
+
+            bar = ProgressBar(0.2)
+            bar.attach()
+            for i in range(100):
+                bar.monitor_progress()
+                time.sleep2(0.1)
+            bar.detach()
+
+        Example 2: monitor also ETA (Estimated Time of Arrival)
+                    notice that the progress bar is detached first and than the lta is updated
+
+            bar = ProgressBar(0.5)
+            for iter_step in range(3):
+                bar.attach()
+                t0 = time.time()
+                for i in range(20):
+                    bar.monitor_progress()
+                    time.sleep(0.2)
+                bar.detach() # detach first
+                bar.update_lta(time.time() - t0) # then update lta
+
+        """
+        self.update_delay = update_delay
+        self.old_time = None
+        self.lta = None  # last time of arrival (for ETA monitoring)
+        self.n_steps = 0  # number of progress bar steps
+
+    def attach(self):
+        # setup progress bar
+        prefix = "  \033[31mProgress\033[0m:    "
+        suffix = "" if self.lta is None else " ETA {0:.3f} s ".format(self.lta)
+        sys.stdout.write(prefix + "[=> " + suffix)
+        sys.stdout.flush()
+        self.old_time = time.time()
+
+    def monitor_progress(self):
+        if self.old_time is None:
+            self.WrongInitializationError.report()
+            raise self.WrongInitializationError
+
+        if time.time() - self.old_time > self.update_delay:
+            # update the progress bar every 'self.update_delay' seconds
+            if self.lta is None:
+                n_back = 2
+                suffix = ""
+            else:
+                eta = self.lta - (self.update_delay * self.n_steps)
+                n_back = 2 + len(" ETA {0:.3f} s ".format(eta))
+                suffix = " ETA {0:.3f} s ".format(eta)
+            sys.stdout.write("\b" * n_back + "=> " + suffix)
+            sys.stdout.flush()
+            self.old_time = time.time()
+            self.n_steps += 1
+
+    def update_lta(self, lta):
+        """ Update LTA with the value of the last time of arrival. """
+        self.lta = lta
+
+    def detach(self):
+        if self.old_time is None:
+            self.WrongInitializationError.report()
+            raise self.WrongInitializationError
+
+        n_back = 2 if self.lta is None \
+            else 2 + len(" ETA {0:.3f} s ".format(self.lta - (self.update_delay * self.n_steps)))
+
+        sys.stdout.write("\b" * n_back + "=]\n")  # this ends the progress bar
+
+        # reset variable:
+        self.n_steps = 0
+
+    class WrongInitializationError(Exception):
+        """ Raised if the progress bar is not correctly initialized """
+
+        @staticmethod
+        def report():
+            instr_0 = 'bar = ProgressBar()'
+            instr_1 = 'bar.attach()'
+            instr_2 = 'bar.monitor_progress()'
+            instr_3 = 'bar.detach()'
+            print("\033[91m\nRemember that the correct procedure to use a ProgressBar is the following:"
+                  "\n{4}{0}\n{4}{1}\n{4}{2}\n{4}{3}\n\033[0m".format(instr_0, instr_1, instr_2, instr_3, ' ' * 3))
+            time.sleep(0.2)

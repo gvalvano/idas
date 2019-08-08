6ce8b37dbf8fec7d36c2e105a3ae7dcd06620dc1
gabriele <gabriele.valvano@gmail.com>
2019-08-08 18:57:15 +0100

    add copyright

--- idas/models/_deprecated/u_net_3d.py
+++ idas/models/_deprecated/u_net_3d.py
@@ -4,6 +4,20 @@ case of the original 2D U-Net from the 2015 paper by Ronneberger et al., at:
   https://arxiv.org/pdf/1505.04597.pdf
 """
 
+#  Copyright 2019 Gabriele Valvano
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import tensorflow as tf
 from tensorflow.layers import conv3d, max_pooling3d, batch_normalization, conv3d_transpose
 
